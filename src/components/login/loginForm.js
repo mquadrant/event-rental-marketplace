109fb200698f4dd14757602f7f4cbf118e9d6fe9
Your Name <you@example.com>
2019-07-08 10:42:50 +0100

    created a users.json file for demo

--- src/components/login/loginForm.js
+++ src/components/login/loginForm.js
@@ -11,7 +11,6 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import useForm from "../../pages/sign-up/helper/useForm";
 
 const useStyles = makeStyles((theme) => ({
   container: {
