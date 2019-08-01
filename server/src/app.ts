b521da9ef660d991122c558fb118bbd5116b07a0
Benjamin Mark <mquadrant@gmail.com>
2019-08-01 19:05:30 +0100

    setup qraphql schema and resolver

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,8 @@ import express,{Request,Response,NextFunction} from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import  logger from "morgan";
+import graphqlHttp from "express-graphql";
+import {buildSchema} from 'graphql'
 
 import indexRouter from "./routes/index";
 import usersRouter from "./routes/users";
@@ -21,6 +23,84 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
+interface Item{
+    _id:string,
+    item_title:string,
+    description:string,
+    price:number,
+    host_name:string,
+    store_address:string,
+    service_type:string,
+    pay_option:string,
+    image_url: [string],
+    date: string | Date
+}
+const EventItems:Item[] = [];
+app.use('/graphql',graphqlHttp({
+  schema:buildSchema(`
+  type EventItem {
+    _id: ID!
+    item_title: String!
+    description: String!
+    price: Float!
+    host_name: String!
+    store_address: String!
+    service_type: String!
+    pay_option: String!
+    image_url:[String!]!
+    date: String!
+  }
+
+  input EventInput {
+    item_title: String!
+    description: String!
+    price: Float!
+    host_name: String!
+    store_address: String!
+    service_type: String!
+    pay_option: String!
+    image_url:[String!]!
+  }
+
+  type RootQuery {
+    eventItems:[EventItem!]!
+  }
+
+  type RootMutation{
+    createEvent(eventItem:EventInput):EventItem
+  }
+
+  schema{
+    query: RootQuery,
+    mutation: RootMutation
+  }
+  `),
+  rootValue:{
+    eventItems:()=>{
+      return EventItems;
+    },
+    createEvent:(args:any)=>{
+      console.log(args.eventItem)
+      const event = {
+        _id:Math.random().toString(),
+        item_title:args.eventItem.item_title,
+        description:args.eventItem.description,
+        price:args.eventItem.price,
+        host_name:args.eventItem.host_name,
+        store_address:args.eventItem.store_address,
+        service_type:args.eventItem.service_type,
+        pay_option:args.eventItem.pay_option,
+        image_url:args.eventItem.image_url,
+        date:new Date().toISOString()
+      }
+      EventItems.push(event);
+      return event;
+    }
+  },
+  graphiql:true
+}))
+
 
 // catch 404 and forward to error handler
 app.use(function(_req:Request, _res:Response, next) {
