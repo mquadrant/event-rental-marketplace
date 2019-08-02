e639af87d961e6bf62c75a91e6302a43fed4b8e7
Benjamin Mark <mquadrant@gmail.com>
2019-08-02 16:34:47 +0100

    saved event item to database

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,6 +6,8 @@ import  logger from "morgan";
 import graphqlHttp from "express-graphql";
 import {buildSchema} from 'graphql'
 
+import EventItem from './models/ItemModel'
+
 import indexRouter from "./routes/index";
 import usersRouter from "./routes/users";
 
@@ -24,19 +26,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-interface Item{
-    _id:string,
-    item_title:string,
-    description:string,
-    price:number,
-    host_name:string,
-    store_address:string,
-    service_type:string,
-    pay_option:string,
-    image_url: [string],
-    date: string | Date
-}
-const EventItems:Item[] = [];
 app.use('/graphql',graphqlHttp({
   schema:buildSchema(`
   type EventItem {
@@ -49,7 +38,8 @@ app.use('/graphql',graphqlHttp({
     service_type: String!
     pay_option: String!
     image_url:[String!]!
-    date: String!
+    createdAt: String!
+    modifiedAt: String!
   }
 
   input EventInput {
@@ -78,12 +68,10 @@ app.use('/graphql',graphqlHttp({
   `),
   rootValue:{
     eventItems:()=>{
-      return EventItems;
+      return [];
     },
-    createEvent:(args:any)=>{
-      console.log(args.eventItem)
-      const event = {
-        _id:Math.random().toString(),
+    createEvent:async(args:any)=>{
+      const item = new EventItem({
         item_title:args.eventItem.item_title,
         description:args.eventItem.description,
         price:args.eventItem.price,
@@ -91,11 +79,16 @@ app.use('/graphql',graphqlHttp({
         store_address:args.eventItem.store_address,
         service_type:args.eventItem.service_type,
         pay_option:args.eventItem.pay_option,
-        image_url:args.eventItem.image_url,
-        date:new Date().toISOString()
+        image_url:args.eventItem.image_url
+      });
+      try{
+        const result = await item.save() ;
+        return result;
+      }catch(err){
+        throw err;
       }
-      EventItems.push(event);
-      return event;
+      
+
     }
   },
   graphiql:true
