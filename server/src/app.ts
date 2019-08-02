bfab4661d0023ec69a3cd3097af52aed4c0b6a05
Benjamin Mark <mquadrant@gmail.com>
2019-08-02 20:55:24 +0100

    converting date to ISO string

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,12 @@
 import createError from "http-errors";
-import express,{Request,Response,NextFunction} from "express";
+import express, { Request, Response, NextFunction } from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
-import  logger from "morgan";
+import logger from "morgan";
 import graphqlHttp from "express-graphql";
-import {buildSchema} from 'graphql'
+import { buildSchema } from "graphql";
 
-import EventItem from './models/ItemModel'
+import EventItem from "./models/ItemModel";
 
 import indexRouter from "./routes/index";
 import usersRouter from "./routes/users";
@@ -26,8 +26,10 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-app.use('/graphql',graphqlHttp({
-  schema:buildSchema(`
+app.use(
+    "/graphql",
+    graphqlHttp({
+        schema: buildSchema(`
   type EventItem {
     _id: ID!
     item_title: String!
@@ -66,49 +68,52 @@ app.use('/graphql',graphqlHttp({
     mutation: RootMutation
   }
   `),
-  rootValue:{
-    eventItems:()=>{
-      return [];
-    },
-    createEvent:async(args:any)=>{
-      const item = new EventItem({
-        item_title:args.eventItem.item_title,
-        description:args.eventItem.description,
-        price:args.eventItem.price,
-        host_name:args.eventItem.host_name,
-        store_address:args.eventItem.store_address,
-        service_type:args.eventItem.service_type,
-        pay_option:args.eventItem.pay_option,
-        image_url:args.eventItem.image_url
-      });
-      try{
-        const result = await item.save() ;
-        return result;
-      }catch(err){
-        throw err;
-      }
-      
-
-    }
-  },
-  graphiql:true
-}))
-
+        rootValue: {
+            eventItems: () => {
+                try {
+                    const items = EventItem.find();
+                    return items;
+                } catch (err) {
+                    throw err;
+                }
+            },
+            createEvent: async (args: any) => {
+                const item = new EventItem({
+                    item_title: args.eventItem.item_title,
+                    description: args.eventItem.description,
+                    price: args.eventItem.price,
+                    host_name: args.eventItem.host_name,
+                    store_address: args.eventItem.store_address,
+                    service_type: args.eventItem.service_type,
+                    pay_option: args.eventItem.pay_option,
+                    image_url: args.eventItem.image_url,
+                });
+                try {
+                    const result = await item.save();
+                    return result;
+                } catch (err) {
+                    throw err;
+                }
+            },
+        },
+        graphiql: true,
+    })
+);
 
 // catch 404 and forward to error handler
-app.use(function(_req:Request, _res:Response, next) {
-  next(createError(404));
+app.use(function(_req: Request, _res: Response, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err:any, req:Request, res:Response, _next:NextFunction) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use(function(err: any, req: Request, res: Response, _next: NextFunction) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 export default app;
--- server/src/models/ItemModel.ts
+++ server/src/models/ItemModel.ts
@@ -1,25 +1,25 @@
-import mongoose, { Schema,Document } from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 
 //Define a Typescript Interface
 export interface IItem extends Document {
-        item_title:string;
-        description:string;
-        price:number;
-        host_name:string;
-        store_address:string;
-        service_type:string;
-        pay_option:string;
-        image_url: [string];
-        createdAt: string | Date;
-        modifiedAt: string | Date; 
-    }
+    item_title: string;
+    description: string;
+    price: number;
+    host_name: string;
+    store_address: string;
+    service_type: string;
+    pay_option: string;
+    image_url: [string];
+    createdAt: string | Date;
+    modifiedAt: string | Date;
+}
 
 //Product Schema
-const eventItemSchema:Schema = new Schema({
+const eventItemSchema: Schema = new Schema({
     item_title: {
         type: String,
         required: [true, "An item must have a title"],
-        trim: true
+        trim: true,
     },
     description: {
         type: String,
@@ -28,7 +28,7 @@ const eventItemSchema:Schema = new Schema({
     },
     price: {
         type: Number,
-        required: [true, "An item must have a price tag"]
+        required: [true, "An item must have a price tag"],
     },
     host_name: {
         type: String,
@@ -62,27 +62,26 @@ const eventItemSchema:Schema = new Schema({
         trim: true,
     },
     createdAt: {
-        type: Date
+        type: Date,
     },
     modifiedAt: {
-        type: Date
-    }
-})
+        type: Date,
+    },
+});
 
 //ADDING date created and date modified using Mongoose
 //Document middleware before saving
-eventItemSchema.pre<IItem>('save',function(next):any{
-        if (this) {
-          let doc = <IItem>this;
-          let now = new Date();
-          if (!doc.createdAt) {
-              doc.createdAt = now;
-          }
-          doc.modifiedAt = now;
+eventItemSchema.pre<IItem>("save", function(next): any {
+    if (this) {
+        let doc = <IItem>this;
+        let now = new Date().toISOString();
+        if (!doc.createdAt) {
+            doc.createdAt = now;
         }
-        next();
-        return this;
+        doc.modifiedAt = now;
+    }
+    next();
+    return this;
 });
 
-
-export default mongoose.model<IItem>('Item',eventItemSchema)
+export default mongoose.model<IItem>("Item", eventItemSchema);
