a8611e9805225c83c6e01e723bb44b56fde631b9
Benjamin Mark <mquadrant@gmail.com>
2019-08-04 10:39:55 +0100

    added booking models

---
+++ server/src/models/bookingModel.ts
@@ -0,0 +1,57 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IBooking extends Document {
+    _doc?: Promise<this>;
+    item: string;
+    user: string;
+    quantity: number;
+    amount: number;
+    status: string;
+    booking_description: string;
+    pickup_date: string | Date;
+    return_date: string | Date;
+    warranty: boolean;
+}
+
+const bookingSchema: Schema = new Schema({
+    item: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Item",
+        },
+    ],
+    user: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        },
+    ],
+    quantity: {
+        type: Number,
+        required: [true, "item quantity must be specified"],
+    },
+    amount: {
+        type: Number,
+    },
+    status: {
+        type: String,
+        default: "reserved",
+    },
+    booking_description: {
+        type: String,
+    },
+    pickup_date: {
+        type: Date,
+        required: [true, "booking pickup date must be specified"],
+    },
+    return_date: {
+        type: Date,
+        required: [true, "booking return date must be specified"],
+    },
+    warranty: {
+        type: Boolean,
+        required: [true, "booking must have a warranty field"],
+    },
+});
+
+export default mongoose.model<IBooking>("Book", bookingSchema, "bookings");
