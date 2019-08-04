ffe17f7143ed21c4b752ac21312b7e0786ceec44
Benjamin Mark <mquadrant@gmail.com>
2019-08-04 10:42:27 +0100

    added timestamp

--- server/src/models/bookingModel.ts
+++ server/src/models/bookingModel.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema, Document } from "mongoose";
+import { Timestamp } from "bson";
 
 export interface IBooking extends Document {
     _doc?: Promise<this>;
@@ -13,45 +14,48 @@ export interface IBooking extends Document {
     warranty: boolean;
 }
 
-const bookingSchema: Schema = new Schema({
-    item: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Item",
+const bookingSchema: Schema = new Schema(
+    {
+        item: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Item",
+            },
+        ],
+        user: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User",
+            },
+        ],
+        quantity: {
+            type: Number,
+            required: [true, "item quantity must be specified"],
         },
-    ],
-    user: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "User",
+        amount: {
+            type: Number,
         },
-    ],
-    quantity: {
-        type: Number,
-        required: [true, "item quantity must be specified"],
+        status: {
+            type: String,
+            default: "reserved",
+        },
+        booking_description: {
+            type: String,
+        },
+        pickup_date: {
+            type: Date,
+            required: [true, "booking pickup date must be specified"],
+        },
+        return_date: {
+            type: Date,
+            required: [true, "booking return date must be specified"],
+        },
+        warranty: {
+            type: Boolean,
+            required: [true, "booking must have a warranty field"],
+        },
     },
-    amount: {
-        type: Number,
-    },
-    status: {
-        type: String,
-        default: "reserved",
-    },
-    booking_description: {
-        type: String,
-    },
-    pickup_date: {
-        type: Date,
-        required: [true, "booking pickup date must be specified"],
-    },
-    return_date: {
-        type: Date,
-        required: [true, "booking return date must be specified"],
-    },
-    warranty: {
-        type: Boolean,
-        required: [true, "booking must have a warranty field"],
-    },
-});
+    { timestamps: true }
+);
 
 export default mongoose.model<IBooking>("Book", bookingSchema, "bookings");
